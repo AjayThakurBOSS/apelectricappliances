728645d3fa3a89b48692e35b78476bda11909d4c
Ajay <ajaykrthakur02@gmail.com>
2025-02-14 11:13:48 +0530

    dotenv updated 10

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.use("/api/v1/feedback", require("./route/feedbackRoutes"));
 //   })
 
 // static file access
-app.use(express.static(path.join(__dirname, './client/build')))
+// app.use(express.static(path.join(__dirname, './client/build')))
 
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-} )
+// app.get('*', function(req, res){
+//     res.sendFile(path.join(__dirname, './client/build/index.html'))
+// } )
 
 
 // Error Handling Middleware
